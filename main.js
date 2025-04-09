a7ba256abba7a15a412df643f6b67d98b5e4eb96
Steven4343 <stevensengimanan777@gmail.com>
2025-04-09 13:02:15 +0300

    initial main

---
+++ main.js
@@ -0,0 +1,10 @@
+//performing task
+function greet(name, lastname) {
+    console.log('hello' + name + ' ' + lastname);
+}
+//calculating function
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
